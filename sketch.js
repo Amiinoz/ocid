8b96f7a149bf327ad34fe706a44704e2df080ecb
Amiinoz <amiinoz@Mohameds-MacBook-Air.local>
2023-07-17 22:26:47 -0600

    clean up

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-//  mapimg = loadImage("https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/46.2037,5.16329,4,0,0/600x1024?access_token=pk.eyJ1IjoibW9zanVuazAwNyIsImEiOiJjbGs1b2kwMW0wMHF1M3JwbjA3dnZ0bHp0In0.RGiWll97yE8muru5skz1eg")
-
 let mapimg;
 let cntrLat = 0;
 let cntrLon = 0;
